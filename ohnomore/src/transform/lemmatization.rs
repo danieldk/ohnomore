5db23a774c86d2675bfcb4ab103fbe49d333567f
Daniël de Kok <me@danieldk.eu>
2017-12-11 14:39:41 +0100

    Refine prefix analysis.
    The prefix analysis would lemmatize 'zustanden stehen' to 'stehen'
    rather than 'zu#stehen'. The longest prefix search found 'zustande' as
    a prefix of 'zustanden'. This prefix was then rejected because the
    remaining form would be the character 'n'.
    
    We now generate all possible prefix sequences for the form, retain
    those that qualify to various heuristics and then take the prefix
    sequence that leaves the shortest form and is the longest prefix
    (e.g. preferring dabei# over da#bei#).

--- ohnomore/src/transform/lemmatization.rs
+++ ohnomore/src/transform/lemmatization.rs
@@ -1,4 +1,5 @@
-use std::collections::HashMap;
+use std::cmp::Ordering;
+use std::collections::{HashMap, VecDeque};
 
 use fst::{IntoStreamer, Set, Streamer};
 use petgraph::Direction;
@@ -149,29 +150,82 @@ where
     }
 }
 
+/// Look for all matches of (prefix)* in the given form. Ideally,
+/// we'd construct a Kleene star automaton of the prefix automaton.
+/// Unfortunately, this functionality is not (yet) provided by the
+/// fst crate. Instead, we repeatedly search prefixes in the set.
+fn prefix_star<'a>(prefix_set: &Set, s: &'a str) -> Vec<(&'a str, Vec<String>)> {
+    let mut result = Vec::new();
+    
+    let mut q = VecDeque::new();
+    q.push_back((s, vec![]));
+
+    while let Some((stripped_s, prefixes)) = q.pop_front() {
+        result.push((stripped_s, prefixes.clone()));
+
+        for prefix in find_prefixes(prefix_set, stripped_s) {
+            let mut prefixes = prefixes.clone();
+            let prefix_len = prefix.len();
+            prefixes.push(prefix);
+            q.push_back((&stripped_s[prefix_len..], prefixes));
+        }
+    }
+
+    result
+}
+
+fn find_prefixes<S>(prefix_set: &Set, s: S) -> Vec<String>
+where
+    S: AsRef<str>,
+{
+    let automaton = PrefixAutomaton::from(s.as_ref());
+
+    let mut prefixes = Vec::new();
+
+    let mut stream = prefix_set.search(&automaton).into_stream();
+    while let Some(prefix) = stream.next() {
+        prefixes.push(prefix.to_owned());
+    }
+
+    prefixes.into_iter().map(|p| String::from_utf8(p).expect("Cannot decode prefix, PrefixAutomaton returned invalid prefix")).collect()
+}
+
 fn longest_prefixes<F, L>(prefix_set: &Set, form: F, lemma: L) -> Vec<String>
 where
     F: AsRef<str>,
     L: AsRef<str>,
 {
     let lemma = lemma.as_ref();
-    let mut stripped = form.as_ref();
+    let form = form.as_ref();
+       
+    let all_prefixes = prefix_star(prefix_set, form);
 
-    let mut prefixes = Vec::new();
-    while let Some(prefix) = longest_prefix(prefix_set, &stripped) {
-        // Prefix should not end with lemma. E.g.:
-        // abgefangen fangen -> ab#fangen, not: ab#gefangen#fangen
-        if lemma.starts_with(&prefix) || prefix.ends_with(&lemma) ||
-            !is_verb(&stripped[prefix.len()..])
-        {
-            break;
+    let prefixes_candidates: Vec<_> = all_prefixes.into_iter().filter(|&(stripped, ref prefixes)| {
+        if prefixes.is_empty() {
+            return true;
         }
 
-        stripped = &stripped[prefix.len()..];
-        prefixes.push(prefix);
-    }
+        let last_prefix = prefixes.last().unwrap();
 
-    prefixes
+        // Do not start stripping parts of the lemma
+        !prefixes.iter().any(|p| lemma.starts_with(p)) &&
+
+        // Prefix should not end with lemma. E.g.:
+        // abgefangen fangen -> ab#fangen, not: ab#gefangen#fangen
+        !last_prefix.ends_with(&lemma) &&
+
+        is_verb(stripped)
+    }).collect();
+
+    prefixes_candidates.into_iter().max_by(|l, r| {
+        match l.0.len().cmp(&r.0.len()) {
+            Ordering::Less => return Ordering::Greater,
+            Ordering::Greater => return Ordering::Less,
+            Ordering::Equal => ()
+        }
+
+        l.1.len().cmp(&r.1.len()).reverse()
+    }).map(|t| t.1).unwrap_or(Vec::new())
 }
 
 fn is_verb<S>(verb: S) -> bool
@@ -180,28 +234,6 @@ where
 {
     // A separable verb with a length shorter than 3 is unlikely.
     verb.as_ref().len() > 2
-}
-
-fn longest_prefix<S>(prefix_set: &Set, s: S) -> Option<String>
-where
-    S: AsRef<str>,
-{
-    let automaton = PrefixAutomaton::from(s.as_ref());
-
-    let mut prefix_candidates = Vec::new();
-
-    let mut stream = prefix_set.search(&automaton).into_stream();
-    while let Some(prefix) = stream.next() {
-        prefix_candidates.push(prefix.to_owned());
-    }
-
-    let longest_prefix = prefix_candidates.into_iter().max_by_key(|p| p.len());
-
-    longest_prefix.map(|p| {
-        String::from_utf8(p).expect(
-            "Cannot decode prefix, PrefixAutomaton returned invalid prefix",
-        )
-    })
 }
 
 #[cfg(test)]
