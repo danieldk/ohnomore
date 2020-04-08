1480079ffdcb7032ccc3622a985cfd6c0c45483a
Daniël de Kok <me@danieldk.eu>
2020-04-08 10:12:09 +0200

    Make most constants crate-private, remove unused constants

--- ohnomore/src/constants.rs
+++ ohnomore/src/constants.rs
@@ -3,41 +3,29 @@ use std::collections::HashSet;
 use lazy_static::lazy_static;
 use maplit::hashset;
 
-pub static REFLEXIVE_PERSONAL_PRONOUN_LEMMA: &str = "#refl";
+pub(crate) static REFLEXIVE_PERSONAL_PRONOUN_LEMMA: &str = "#refl";
 
-pub static SEPARABLE_PARTICLE_POS: &str = "PTKVZ";
+pub(crate) static SEPARABLE_PARTICLE_POS: &str = "PTKVZ";
 
-pub static PUNCTUATION_PREFIX: &str = "$";
-pub static VERB_PREFIX: &str = "V";
+pub(crate) static PUNCTUATION_PREFIX: &str = "$";
 
-pub static ARTICLE_TAG: &str = "ART";
-pub static ATTRIBUTIVE_POSSESIVE_PRONOUN_TAG: &str = "PPOSAT";
-pub static SUBST_POSSESIVE_PRONOUN_TAG: &str = "PPOSS";
-pub static FOREIGN_WORD_TAG: &str = "FM";
-pub static NAMED_ENTITY_TAG: &str = "NE";
-pub static NON_WORD_TAG: &str = "XY";
-pub static NOUN_TAG: &str = "NN";
-pub static PARTICIPLE_TAG: &str = "VVPP";
-pub static PERSONAL_PRONOUN_TAG: &str = "PPER";
-pub static REFLEXIVE_PERSONAL_PRONOUN_TAG: &str = "PRF";
-pub static SUBST_REL_PRONOUN: &str = "PRELS";
-pub static ATTR_REL_PRONOUN: &str = "PRELAT";
-pub static TRUNCATED_TAG: &str = "TRUNC";
-pub static ZU_INFINITIVE_VERB: &str = "VVIZU";
-pub static INFINITIVE_VERB_TAG: &str = "VVINF";
+pub(crate) static ARTICLE_TAG: &str = "ART";
+pub(crate) static ATTRIBUTIVE_POSSESIVE_PRONOUN_TAG: &str = "PPOSAT";
+pub(crate) static SUBST_POSSESIVE_PRONOUN_TAG: &str = "PPOSS";
+pub(crate) static FOREIGN_WORD_TAG: &str = "FM";
+pub(crate) static NAMED_ENTITY_TAG: &str = "NE";
+pub(crate) static NON_WORD_TAG: &str = "XY";
+pub(crate) static NOUN_TAG: &str = "NN";
+pub(crate) static PERSONAL_PRONOUN_TAG: &str = "PPER";
+pub(crate) static REFLEXIVE_PERSONAL_PRONOUN_TAG: &str = "PRF";
+pub(crate) static SUBST_REL_PRONOUN: &str = "PRELS";
+pub(crate) static ATTR_REL_PRONOUN: &str = "PRELAT";
+pub(crate) static TRUNCATED_TAG: &str = "TRUNC";
+pub(crate) static ZU_INFINITIVE_VERB: &str = "VVIZU";
 
-pub static SUBSTITUTING_INDEF_PRONOUN: &str = "PIS";
-pub static ATTRIBUTING_INDEF_PRONOUN_WITHOUT_DET: &str = "PIAT";
-pub static ATTRIBUTING_INDEF_PRONOUN_WITH_DET: &str = "PIDAT";
-
-pub static ADVERBIAL_RELATION: &str = "ADV";
-pub static AUXILIARY_RELATION: &str = "AUX";
-pub static CONJ_COMPLEMENT_RELATION: &str = "CJ";
-pub static COORDINATION_RELATION: &str = "KON";
-pub static PUNCTUATION_RELATION: &str = "-PUNCT-";
-pub static SEP_VERB_PREFIX_RELATION: &str = "AVZ";
-
-pub static PASSIVE_VERB_LEMMA: &str = "werden";
+pub(crate) static SUBSTITUTING_INDEF_PRONOUN: &str = "PIS";
+pub(crate) static ATTRIBUTING_INDEF_PRONOUN_WITHOUT_DET: &str = "PIAT";
+pub(crate) static ATTRIBUTING_INDEF_PRONOUN_WITH_DET: &str = "PIDAT";
 
 lazy_static! {
     pub static ref NO_LEMMA_TAGS: HashSet<&'static str> = hashset! {
@@ -64,12 +52,12 @@ lazy_static! {
         "PTKNEG",
     };
 
-    pub static ref LEMMA_IS_FORM_PRESERVE_CASE_TAGS: HashSet<&'static str> = hashset! {
+    pub(crate) static ref LEMMA_IS_FORM_PRESERVE_CASE_TAGS: HashSet<&'static str> = hashset! {
         FOREIGN_WORD_TAG,
     };
 
     /// Part-of-speech tags that have special (non-word) lemmas.
-    pub static ref SPECIAL_LEMMA_TAGS: HashSet<&'static str> = hashset! {
+    pub(crate) static ref SPECIAL_LEMMA_TAGS: HashSet<&'static str> = hashset! {
         "ART",
         "PDS",
         "PPER",
@@ -79,21 +67,14 @@ lazy_static! {
     };
 }
 
-pub fn is_verb<S>(tag: S) -> bool
+pub(crate) fn is_verb<S>(tag: S) -> bool
 where
     S: AsRef<str>,
 {
     tag.as_ref().starts_with('V')
 }
 
-pub fn is_finite_verb<S>(tag: S) -> bool
-where
-    S: AsRef<str>,
-{
-    tag.as_ref().starts_with('V') && tag.as_ref().ends_with("FIN")
-}
-
-pub fn is_separable_verb<S>(tag: S) -> bool
+pub(crate) fn is_separable_verb<S>(tag: S) -> bool
 where
     S: AsRef<str>,
 {
